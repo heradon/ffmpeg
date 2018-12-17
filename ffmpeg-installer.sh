a4279e93c07200cc918b038fb2508429fb226c69
heradon <webmaster@rmatthes.de>
2018-12-17 01:04:32 +0100

    Update ffmpeg-installer.sh
    added arguments

--- ffmpeg-installer.sh
+++ ffmpeg-installer.sh
@@ -1,5 +1,49 @@
 #!/usr/bin/env bash
 
+### CMD Arguments ###
+
+ENVIRONMENT="dev"
+DB_PATH="/data/db"
+
+function usage()
+{
+    echo "if this was a real script you would see something useful here"
+    echo ""
+    echo "./simple_args_parsing.sh"
+    echo "\t-h --help"
+    echo "\t--environment=$ENVIRONMENT"
+    echo "\t--db-path=$DB_PATH"
+    echo ""
+}
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        --environment)
+            ENVIRONMENT=$VALUE
+            ;;
+        --db-path)
+            DB_PATH=$VALUE
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+echo "ENVIRONMENT is $ENVIRONMENT";
+echo "DB_PATH is $DB_PATH";
+
+### CMD Arguments ###
+
 # Installs ffmpeg from source (HEAD) with libaom and libx265, as well as a few
 # other common libraries
 # binary will be at ~/bin/ffmpeg
